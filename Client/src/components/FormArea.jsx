104f522a64a9b61c42cdb98ae17b88153cca6fe2
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2024-09-12 22:18:15 +0530

    Feat:Login form and dashboard section

--- Client/src/components/FormArea.jsx
+++ Client/src/components/FormArea.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { adminEmail, adminPassword } from "../../Utils/constants.js";
+import { adminEmail, adminPassword } from "../../Utils/GeneralValues";
 import { useNavigate } from "react-router-dom";
 
 const FormArea = () => {
