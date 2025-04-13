9010d0545f248b1c771c8078d50671ad926186dd
Aritra Chakraborty <aritra.chakraborty203@gmail.com>
2025-04-13 14:03:25 +0530

    Update:update in image

--- Client/Utils/GeneralValues.js
+++ Client/Utils/GeneralValues.js
@@ -2,4 +2,4 @@ export const adminEmail = "aritra.chakraborty203@gmail.com";
 export const adminPassword = "Aritra0307@3243";
 export const CMSLink = "/generalCMS";
 export const TEST_API_BASE_URL = "http://localhost:8080/";
-export const PRODUCTION_API_BASE_URL = "https://api.urtechguru.online/";
+export const PRODUCTION_API_BASE_URL = "https://api.profiletvault.online/";
